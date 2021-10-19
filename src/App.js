007ba920028e907cf60de52178bac892a14a22a1
juliainkaxo <89917375+juliainkaxo@users.noreply.github.com>
2021-10-19 13:04:36 +0300

    pliis mene

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const URL = 'http://localhost/todo/';
+//jee
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const [editTask, setEditTask] = useState(null);
+  const [editDescription, setEditDescription] = useState('');
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setTasks(response.data)
+      })
+  }, [])
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ description: task })
+    axios.post(URL + 'add.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        setTasks(tasks => [...tasks, response.data]);
+        setTask('');
+      }).catch(error => {
+        alert(error.response.data.error)
+      });
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = tasks.filter((item) => item.id !== id);
+        setTasks(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+
+  function setEditedTask(task) {
+    setEditTask(task);
+    setEditDescription(task?.description);
+  }
+
+  function update(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ id: editTask.id, description: editDescription })
+    axios.post(URL + 'update.php', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        tasks[(tasks.findIndex(task => task.id === editTask.id))].description = editDescription;
+        setTasks([...tasks]);
+        setEditedTask(null);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Todo List</h3>
+      <form onSubmit={save}>
+        <label>New Task</label>
+        <input value={task} onChange={e => setTask(e.target.value)} />
+        <button>Save</button>
+      </form>
+
+      <ol>
+        {tasks.map(task => (
+          <li key={task.id}>{editTask?.id !== task.id && task.description}
+            {editTask?.id === task.id &&
+              <form onSubmit={update}>
+                <input value={editDescription} onChange={e => setEditDescription(e.target.value)} />
+                <button>Save</button>
+                <button type='button' onClick={() => setEditTask(null)}>Cancel</button>
+              </form>
+            }
+            <a className='delete' onClick={() => remove(task.id)} href='#'> Delete </a>
+            {editTask === null &&
+              <a className='delete' onClick={() => setEditedTask(task)} href='#'> Edit </a>
+            }
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
